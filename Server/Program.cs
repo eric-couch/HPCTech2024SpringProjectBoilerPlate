6e6daeff613da6b2e2d2b7ab1da26aca9ec7d6d1
Eric Couch <ericcouch@yahoo.com>
2024-03-14 13:20:32 -0500

    extended identity application user with first name and last name.  added fields to registration form.

--- Server/Program.cs
+++ Server/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
 using HPCTech2024SpringProjectBoilerPlate.Server.Data;
 using HPCTech2024SpringProjectBoilerPlate.Server.Models;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
