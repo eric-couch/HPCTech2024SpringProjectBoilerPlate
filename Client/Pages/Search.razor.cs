5a9adeb943150770e09d4684537738db4b7d2c29
Eric Couch <ericcouch@yahoo.com>
2024-03-15 14:28:55 -0500

    added search page

---
+++ Client/Pages/Search.razor.cs
@@ -0,0 +1,45 @@
+﻿using HPCTech2024SpringProjectBoilerPlate.Shared;
+using Microsoft.AspNetCore.Components;
+using System.ComponentModel;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace HPCTech2024SpringProjectBoilerPlate.Client.Pages;
+
+public partial class Search
+{
+    [Inject]
+    HttpClient Http { get; set; }
+
+    private string searchTerm;
+    private MovieSearchResult searchResults = new MovieSearchResult();
+    private IQueryable<MovieSearchResultItem> OMDBMovies { get; set; }
+    int TotalItems = 0;
+    private readonly string OMDBAPIUrl = "https://www.omdbapi.com/?apikey=";
+    private readonly string OMDBAPIKey = "86c39163";
+
+    private async Task SearchOMDB()
+    {
+        try
+        {
+            searchResults = await Http.GetFromJsonAsync<MovieSearchResult>($"{OMDBAPIUrl}{OMDBAPIKey}&s={searchTerm}");
+            if (searchResults is not null)
+            {
+                OMDBMovies = searchResults.Search.AsQueryable();
+                TotalItems = int.Parse(searchResults.totalResults);
+                StateHasChanged();
+            }
+            else
+            {
+                // log no results
+                // display no results to user in toast or in grid message 
+            }
+        }
+        catch (Exception e)
+        {
+            // unpack errors to display to user (user friendly message = "something went wrong")
+            // log errors
+            Console.WriteLine(e.Message);
+        }
+    }
+}
