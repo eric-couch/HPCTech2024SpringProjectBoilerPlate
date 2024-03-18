75d0d86fd69fab8beb53019d94719efeab8274a8
Eric Couch <ericcouch@yahoo.com>
2024-03-18 14:10:39 -0500

    added real grid (syncfusion) to search page.

--- Client/Pages/Search.razor.cs
+++ Client/Pages/Search.razor.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Components;
 using System.ComponentModel;
 using System.Net.Http.Json;
 using System.Text.Json;
+using Syncfusion.Blazor.Grids;
+using Syncfusion.Blazor.Navigations;
 
 namespace HPCTech2024SpringProjectBoilerPlate.Client.Pages;
 
@@ -13,19 +15,29 @@ public partial class Search
 
     private string searchTerm;
     private MovieSearchResult searchResults = new MovieSearchResult();
-    private IQueryable<MovieSearchResultItem> OMDBMovies { get; set; }
+    private List<MovieSearchResultItem> OMDBMovies { get; set; }
     int TotalItems = 0;
+    int page = 1;
     private readonly string OMDBAPIUrl = "https://www.omdbapi.com/?apikey=";
     private readonly string OMDBAPIKey = "86c39163";
+    public SfGrid<MovieSearchResultItem> movieGrid;
+    public SfPager Page;
+
+
+    public async Task PageClick(PagerItemClickEventArgs args)
+    {
+        page = args.CurrentPage;
+        await SearchOMDB();
+    }
 
     private async Task SearchOMDB()
     {
         try
         {
-            searchResults = await Http.GetFromJsonAsync<MovieSearchResult>($"{OMDBAPIUrl}{OMDBAPIKey}&s={searchTerm}");
+            searchResults = await Http.GetFromJsonAsync<MovieSearchResult>($"{OMDBAPIUrl}{OMDBAPIKey}&s={searchTerm}&page={page}");
             if (searchResults is not null)
             {
-                OMDBMovies = searchResults.Search.AsQueryable();
+                OMDBMovies = searchResults.Search.ToList();
                 TotalItems = int.Parse(searchResults.totalResults);
                 StateHasChanged();
             }
--- Client/Program.cs
+++ Client/Program.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using HPCTech2024SpringProjectBoilerPlate.Client;
+using Syncfusion.Blazor;
+
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NBaF5cXmZCfUx0QHxbf1x0ZFREal1STnZaUj0eQnxTdEFjWn5WcXZURWBZU0V0Xg==");
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -14,5 +17,5 @@ builder.Services.AddHttpClient("HPCTech2024SpringProjectBoilerPlate.ServerAPI", 
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("HPCTech2024SpringProjectBoilerPlate.ServerAPI"));
 
 builder.Services.AddApiAuthorization();
-
+builder.Services.AddSyncfusionBlazor();
 await builder.Build().RunAsync();
